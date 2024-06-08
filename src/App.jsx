55deb0cf5819a48c9ac4fa9311eccea8d579889e
Pratik Waware <pratikwavare919@gmail.com>
2024-06-09 01:00:37 +0530

    ag-grid added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import './App.css'
+// import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
+// import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
+import './assets/css/ag-grid-theme-builder.css'
 import { Button } from './components/ui/button'
+import CustomTable from './components/table/CustomTable';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +16,7 @@ function App() {
           Hello world!
         </h1>
         <Button>Click me</Button>
+        <CustomTable />
       </div>
     </>
   )
